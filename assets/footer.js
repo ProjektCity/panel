d6a50e70b242d551ecaf3e985d8f8d06729fe50b
itsmarian <projektcityofficial@gmail.com>
2026-03-28 00:47:25 +0100

    Fixed issues on mobile devices and updated design to match brand colors and styles.

--- assets/footer.js
+++ assets/footer.js
@@ -6,6 +6,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     footer.innerHTML = `
         <p class="small">This is an internal site of Projekt City. Unauthorized access is prohibited and may result in legal action.</p>
+        <p>
+            <a data-linkout="https://projektcity.com/legal/privacy" data-target="_blank" data-i18n="footer_privacy" style="font-size: 1.1rem;">Privacy</a> • 
+            <a data-linkout="https://projektcity.com/legal/terms" data-target="_blank" data-i18n="footer_terms" style="font-size: 1.1rem;">Terms</a>
+        </p>
         <div>
             <p>&copy; ${year} ${branding} | <span>All rights reserved!</span></p>
             <p class="f-colored"><span>Made with ❤️ in</span>&nbsp;<a style="color:#000000;">GE</a><a style="color:#DD0000;">RMA</a><a style="color:#FFCE00;">NY</a></p>
--- assets/header.js
+++ assets/header.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     header.innerHTML = `
         <div class="header-inner">
-            <div class="header-box" data-linkout="/"><div class="flex-container"><div class="header-logo"></div><div class="panel-name"><span>Projekt City</span> Panel</div></div></div>
-            <div class="header-box" style="display: flex; justify-content: center; width: 50%;"><div class="header-menu"><div class="header-item"><a href="/">Dashboard</a></div><div class="header-item"><a href="/tasks.html">Tasks</a></div><div class="header-item"><a href="/changelog.html">Changelog</a></div><div class="header-item"><a href="/inbox.html">Inbox</a></div></div></div>
+            <div class="header-box customwidth" data-linkout="/"><div class="flex-container"><div class="header-logo"></div><div class="panel-name"><span>Projekt City</span> Panel</div></div></div>
+            <div class="header-box hideonmobile" style="display: flex; justify-content: center; width: 50%;"><div class="header-menu"><div class="header-item"><a href="/">Dashboard</a></div><div class="header-item"><a href="/tasks.html">Tasks</a></div><div class="header-item"><a href="/changelog.html">Changelog</a></div><div class="header-item"><a href="/inbox.html">Inbox</a></div></div></div>
             <div class="header-box flex-container justify-content--end">
                 <button id="logout-btn" class="btn btn-ghost"><i class="fa-solid fa-right-from-bracket"></i></button>
                 <div class="header-nav-menu menu-toggle" id="mobile-menu"><div class="menu-icon"><div class="line line-1"></div><div class="line line-2"></div><div class="line line-3"></div></div></div>
